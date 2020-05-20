d49f24c525275c6507b97cb6b8deaaf7fa5d4e41
carloshg90 <carloshg90@github.com>
2020-05-20 15:56:27 +0200

    canvis noms controladors

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Auth::routes();
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/politicaPrivacitat', 'politicaPrivacitat@show');
+Route::get('/politicaPrivacitat', 'PoliticaPrivacitat@show');
 
 //Rutes que només son accessibles si estem loguejats
 Route::group(['middleware' => 'auth'], function() {
@@ -27,58 +27,58 @@ Route::get('/home', 'HomeController@index');
  */
     Route::get('/homeTreballador', 'TreballadorController@show');
     Route::get('/homeEmpresa', 'EmpresaController@show');
-    Route::get('/perfil', 'perfil@show');
-    Route::get('/editarPerfil', 'editarPerfil@show');
-    Route::put('/editarPerfil', 'editarPerfil@editar');
-    Route::get('/canviarContrassenya', 'canviarContrassenyaController@show');
-    Route::put('/canviarContrassenya', 'canviarContrassenyaController@editar');
-    Route::get('/perfilAlie/{id}', 'perfil@showOther');
+    Route::get('/perfil', 'Perfil@show');
+    Route::get('/editarPerfil', 'EditarPerfil@show');
+    Route::put('/editarPerfil', 'EditarPerfil@editar');
+    Route::get('/canviarContrassenya', 'CanviarContrassenyaController@show');
+    Route::put('/canviarContrassenya', 'CanviarContrassenyaController@editar');
+    Route::get('/perfilAlie/{id}', 'Perfil@showOther');
 /**
  * Rutes relacionades amb la creacio, vista, i modificació de les ofertes de treball
  */
-    Route::get('/ofertes', 'ofertaController@show');
-    Route::put('/ofertes', 'ofertaController@crear');
-    Route::put('/ofertesCreades/{id}', 'ofertaController@editarOferta');
-    Route::delete('/ofertesCreades/{id}', 'ofertaController@borrarOferta');
-    Route::get('/editarOferta/{id}', 'ofertaController@showEditar');
-    Route::put('/editarOferta/{id}', 'ofertaController@editarOferta');
+    Route::get('/ofertes', 'OfertaController@show');
+    Route::put('/ofertes', 'OfertaController@crear');
+    Route::put('/ofertesCreades/{id}', 'OfertaController@editarOferta');
+    Route::delete('/ofertesCreades/{id}', 'OfertaController@borrarOferta');
+    Route::get('/editarOferta/{id}', 'OfertaController@showEditar');
+    Route::put('/editarOferta/{id}', 'OfertaController@editarOferta');
     //Mostrar les meves ofertes
-    Route::get('/ofertesCreades', 'ofertaController@mevesOfertes');
-    Route::get('/ofertesCreadesAjax', 'ofertaController@mevesOfertesAjax');//Ajax
-    Route::delete('/esborrarMevaOferta/{id}', 'ofertaController@deleteOferta');//Ajax
+    Route::get('/ofertesCreades', 'OfertaController@mevesOfertes');
+    Route::get('/ofertesCreadesAjax', 'OfertaController@mevesOfertesAjax');//Ajax
+    Route::delete('/esborrarMevaOferta/{id}', 'OfertaController@deleteOferta');//Ajax
     //Mostrar ofertes de la zona
-    Route::get('/ofertesAjax', 'ofertaController@mostraOfertesAjax');//Ajax
-    Route::post('/ofertesAjaxSeguir/{id}', 'ofertaController@seguirAjax');//Ajax
-    Route::get('/ofertesSectorZona', 'ofertaController@mostraOfertesSector');
+    Route::get('/ofertesAjax', 'OfertaController@mostraOfertesAjax');//Ajax
+    Route::post('/ofertesAjaxSeguir/{id}', 'OfertaController@seguirAjax');//Ajax
+    Route::get('/ofertesSectorZona', 'OfertaController@mostraOfertesSector');
     //Mostrar ofertes de les empreses que segueixo
-    Route::get('/ofertesSeguitsAjax', 'seguidorController@mostraOfertesSeguitsAjax');//Ajax
-    Route::delete('/ofertesAjaxBorrar/{id}', 'seguidorController@esborrarAjax');//Ajax
-    Route::get('/ofertesSeguits', 'seguidorController@mostraOfertesSeguits');
+    Route::get('/ofertesSeguitsAjax', 'SeguidorController@mostraOfertesSeguitsAjax');//Ajax
+    Route::delete('/ofertesAjaxBorrar/{id}', 'SeguidorController@esborrarAjax');//Ajax
+    Route::get('/ofertesSeguits', 'SeguidorController@mostraOfertesSeguits');
 
 /**
  * Rutes relacionades amb el sistema de missatgeria via mail
  */
     //Formulari en relació a una oferta
-    Route::get('/formMail/{id}','mailController@index');
+    Route::get('/formMail/{id}','MailController@index');
     //Ruta per enviar el correu en relació a una oferta
-    Route::post('/send','mailController@send');
+    Route::post('/send','MailController@send');
     //Mostrar i eliminar els missatges que he enviat a les empreses
-    Route::get('/correusEnviats', 'mailController@show');
-    Route::get('/correusEnviatsEmpresa', 'mailController@show2');
-    Route::get('/correusEnviatsAjax', 'mailController@rebreCorreus');//Ajax
-    Route::delete('/esborrarMissatge/{id}', 'mailController@esborrarMissatge');//Ajax
+    Route::get('/correusEnviats', 'MailController@show');
+    Route::get('/correusEnviatsEmpresa', 'MailController@show2');
+    Route::get('/correusEnviatsAjax', 'MailController@rebreCorreus');//Ajax
+    Route::delete('/esborrarMissatge/{id}', 'MailController@esborrarMissatge');//Ajax
 
     //Formulari de contacte comú
-    Route::get('/formulariContacte/{id}','mailController@index2');
+    Route::get('/formulariContacte/{id}','MailController@index2');
     //Ruta per enviar el correu de contacte
-    Route::post('/enviar','mailController@enviar');
+    Route::post('/enviar','MailController@enviar');
 
 /**
  * Rutes relacionades amb l'apartat de buscadors
  */
-    Route::get('/buscarTreballadors','cercadorController@showTreballadors');
-    Route::get('/buscarEmpreses','cercadorController@showEmpreses');
-    Route::get('/obtenirTreballadors','cercadorController@getTreballadorsAjax');//Ajax
-    Route::get('/obtenirEmpreses','cercadorController@getEmpresesAjax');//Ajax
+    Route::get('/buscarTreballadors','CercadorController@showTreballadors');
+    Route::get('/buscarEmpreses','CercadorController@showEmpreses');
+    Route::get('/obtenirTreballadors','CercadorController@getTreballadorsAjax');//Ajax
+    Route::get('/obtenirEmpreses','CercadorController@getEmpresesAjax');//Ajax
 
 });
